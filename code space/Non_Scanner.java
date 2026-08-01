111a4da4259245fe85260cd9dd9b8d3eb6e7fa63
Om Abhinav <omabhinav4@gmail.com>
2026-08-01 10:20:10 +0530

    calling classes

---
+++ code space/Non_Scanner.java
@@ -0,0 +1,30 @@
+class Ex1
+{
+	private String name;
+	private int marks = -1;
+	
+	public String getName(){return name;}
+	
+	public int getMarks(){return marks;}
+	
+	public void setName(String name){this.name = name;}
+	
+	public void setMarks(int marks)
+	{
+		if(marks >= 0 && marks <= 100){this.marks = marks;}
+		else{System.out.println("Invalid marks");}
+	}
+}
+public class Ex
+{
+	public static void main(String args[])
+	{
+		Ex1 obj = new Ex1();
+		obj.setName("Varma");
+		obj.setMarks(95);
+		
+		System.out.println(obj.getName());
+		if(obj.getMarks() == -1){System.out.println("NA");}
+		else {System.out.println(obj.getMarks());}
+	}
+}
